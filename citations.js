de6c6c2ef0884d1fd02e404d742fbc0de7425229
MarinMalouin <p5oc@free.fr>
2020-01-03 21:25:03 +0100

    repeat program

--- citations.js
+++ citations.js
@@ -37,48 +37,65 @@ const thirdArray = [
     "OpenClassrooms",
 ];
 
+function main() {
+    let choice = '';
+    while (choice !== 'basic' && choice !== 'foolish') {
+        choice = window.prompt("Quel mode voulez vous : Basic ou Foolish?", "foolish").toLowerCase();
+    }
+
+    let numChoosed = 0;
+    while (numChoosed <= 0 || numChoosed > 5) {
+        numChoosed = window.prompt("Combien de citations entre 1 & 5 voulez vous (default : 1)?", 1);
+        if (isNaN(numChoosed)) {
+            numChoosed = 0;
+        }
+    }
+
+    if (choice === 'basic') {
+        for(var i = numChoosed; i > 0; i--) {
+            let rand = random();
+
+            let firstPart = firstArray[rand];
+            let secondPart = secondArray[rand];
+            let thirdPart = thirdArray[rand];
+        
+            let citation = `${firstPart} ${secondPart} ${thirdPart}`;
+        
+            console.log(citation);
+        }
+    } else {
+        for(var i = numChoosed; i > 0; i--) {
+            let randFirst = random();
+            let randSecond = random();
+            let randThird = random();
+        
+            let firstPart = firstArray[randFirst];
+            let secondPart = secondArray[randSecond];
+            let thirdPart = thirdArray[randThird];
+        
+            let citation = `${firstPart} ${secondPart} ${thirdPart}`;
+        
+            console.log(citation);
+        }
+    }
+}
+
 function random () {
     return Math.floor(Math.random()*10);
 }
 
-let choice = '';
-while (choice !== 'basic' && choice !== 'foolish') {
-    choice = window.prompt("Quel mode voulez vous : Basic ou Foolish?").toLowerCase();
-}
-
-let numChoosed = 0;
-while (numChoosed <= 0 || numChoosed > 5) {
-    numChoosed = window.prompt("Combien de citations entre 1 & 5 voulez vous (default : 1)?", 1);
-    if (isNaN(numChoosed)) {
-        numChoosed = 0;
+main();
+let again = ''
+while (again !== 'non') {
+    again = window.prompt("Voulez vous recommencer?", "non").toLowerCase();
+    switch (again) {
+        case 'oui':
+            main();
+            again = '?';
+            break;
+        case 'non':
+            break;
+        default:
+            break;
     }
 }
-
-if (choice === 'basic') {
-    for(var i = numChoosed; i > 0; i--) {
-        let rand = random();
-
-        let firstPart = firstArray[rand];
-        let secondPart = secondArray[rand];
-        let thirdPart = thirdArray[rand];
-    
-        let citation = `${firstPart} ${secondPart} ${thirdPart}`;
-    
-        console.log(citation);
-    }
-} else {
-    for(var i = numChoosed; i > 0; i--) {
-        let randFirst = random();
-        let randSecond = random();
-        let randThird = random();
-    
-        let firstPart = firstArray[randFirst];
-        let secondPart = secondArray[randSecond];
-        let thirdPart = thirdArray[randThird];
-    
-        let citation = `${firstPart} ${secondPart} ${thirdPart}`;
-    
-        console.log(citation);
-    }
-}
-
